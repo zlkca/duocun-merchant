41f4220438098ab92c340812056b8c02479a61a9
zlkca <likzhang@gmail.com>
2020-02-01 13:32:50 -0500

    update account detail page

--- src/app/account/balance-page/balance-page.component.ts
+++ src/app/account/balance-page/balance-page.component.ts
@@ -7,9 +7,7 @@ import { AccountService } from '../../account/account.service';
 import { Role } from '../../account/account.model';
 import * as moment from 'moment';
 import { TransactionService } from '../../transaction/transaction.service';
-import { OrderService } from '../../order/order.service';
-import { IOrder } from '../../order/order.model';
-import { ITransaction } from '../../transaction/transaction.model';
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-balance-page',
@@ -17,7 +15,7 @@ import { ITransaction } from '../../transaction/transaction.model';
   styleUrls: ['./balance-page.component.scss']
 })
 export class BalancePageComponent implements OnInit {
-  displayedColumns: string[] = ['created', 'paid', 'received', 'balance'];
+  displayedColumns: string[] = ['created', 'description', 'paid', 'received', 'balance'];
   dataSource: MatTableDataSource<IMerchantPaymentData>;
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
@@ -26,10 +24,10 @@ export class BalancePageComponent implements OnInit {
 
   receivables;
   payments;
+  lang = environment.language;
 
   constructor(
     private accountSvc: AccountService,
-    private orderSvc: OrderService,
     private transactionSvc: TransactionService
   ) {
 
@@ -59,106 +57,122 @@ export class BalancePageComponent implements OnInit {
     }), {});
   }
 
-  groupBySameDay(items, key) {
+  groupByDelivered(items) {
     const groups = {};
     items.map(it => {
-      let date = null;
+      let delivered = null;
       if (it.hasOwnProperty('delivered')) {
-        date = moment(it.delivered).set({ hour: 0, minute: 0, second: 0, millisecond: 0 });
+        delivered = moment(it.delivered);
+        const dt = Object.keys(groups).find(x => moment(x).isSame(delivered, 'day'));
+        if (dt) {
+          groups[dt].push(it);
+        } else {
+          groups[delivered.toISOString()] = [it];
+        }
       } else {
-        date = moment(it[key]).set({ hour: 0, minute: 0, second: 0, millisecond: 0 });
-      }
-      const dt = Object.keys(groups).find(x => moment(x).isSame(date, 'day'));
-
-      if (dt) {
-        groups[dt].push(it);
-      } else {
-        groups[date.toISOString()] = [it];
+        console.log('No delivered Transaction:' + it._id);
       }
     });
-
     return groups;
   }
 
-  reload(merchantId: string) {
-    const q = {
-      '$or': [{ fromId: merchantId }, { toId: merchantId }],
-      status: { $ne: 'del' },
-      action: { $ne: 'duocun cancel order from merchant' }
+  getDescription(t, merchantAccountId) {
+    if (t.items && t.items.length > 0) {
+      if (environment.language === 'en') {
+        return 'client cancel order';
+      } else {
+        return '客户撤销订单';
+      }
+    } else {
+      return t.toId === merchantAccountId ? t.fromName : t.toName;
+    }
+  }
+
+  reload(merchantAccountId: string) {
+    const qCredit = {
+      fromId: merchantAccountId
     };
-    this.transactionSvc.quickFind(q).pipe(takeUntil(this.onDestroy$)).subscribe(ts => {
-      let list = [];
-      let balance = 0;
-      const credits = ts.filter(t => t.fromId === merchantId);
-      const debits = ts.filter(t => t.toId === merchantId);
-      const receivables = this.groupBySameDay(credits, 'created');
-      Object.keys(receivables).map(dt => {
-        const its = receivables[dt];
-        let amount = 0;
-        its.map(it => { amount += it.amount; });
-        list.push({ created: dt, description: '', type: 'credit', paid: amount, received: 0, balance: 0 });
-      });
 
-      debits.map(t => {
-        const description = t.fromId === merchantId ? t.toName : t.fromName;
-        list.push({ created: t.created, description: description, type: 'debit', paid: 0, received: t.amount, balance: 0 });
-      });
+    const qDebit = {
+      toId: merchantAccountId
+    };
 
-      list = list.sort((a: any, b: any) => {
-        const aMoment = moment(a.created);
-        const bMoment = moment(b.created);
-        if (aMoment.isSame(bMoment, 'day')) {
-          if (a.type === 'debit') {
-            return 1;
+    this.transactionSvc.quickFind(qCredit).pipe(takeUntil(this.onDestroy$)).subscribe(credits => {
+      this.transactionSvc.quickFind(qDebit).pipe(takeUntil(this.onDestroy$)).subscribe(debits => {
+        let list = [];
+        let balance = 0;
+        const receivables = this.groupByDelivered(credits);
+        Object.keys(receivables).map(dt => {
+          const its = receivables[dt];
+          let amount = 0;
+          its.map(it => { amount += it.amount; });
+          list.push({ created: dt, description: '', type: 'credit', paid: amount, received: 0, balance: 0, items: null });
+        });
+
+        debits.map(t => {
+          const description = this.getDescription(t, merchantAccountId);
+          list.push({
+            created: t.created, description: description, type: 'debit', paid: 0, received: t.amount, balance: 0,
+            items: t.items ? t.items : null
+          });
+        });
+
+        list = list.sort((a: any, b: any) => {
+          const aMoment = moment(a.created);
+          const bMoment = moment(b.created);
+          if (aMoment.isSame(bMoment, 'day')) {
+            if (a.type === 'debit') {
+              return 1;
+            } else {
+              if (aMoment.isAfter(bMoment)) {
+                return 1; // a to bottom
+              } else {
+                return -1;
+              }
+            }
           } else {
             if (aMoment.isAfter(bMoment)) {
-              return 1; // a to bottom
+              return 1;
             } else {
               return -1;
             }
           }
-        } else {
-          if (aMoment.isAfter(bMoment)) {
-            return 1;
+        });
+
+        list.map(item => {
+          if (item.type === 'credit') {
+            balance += item.paid;
           } else {
-            return -1;
+            balance -= item.received;
           }
-        }
-      });
+          item.balance = balance;
+        });
 
-      list.map(item => {
-        if (item.type === 'credit') {
-          balance += item.paid;
-        } else {
-          balance -= item.received;
-        }
-        item.balance = balance;
-      });
-
-      const rows = list.sort((a: any, b: any) => {
-        const aMoment = moment(a.created);
-        const bMoment = moment(b.created);
-        if (aMoment.isSame(bMoment, 'day')) {
-          if (a.type === 'debit') {
-            return -1;
+        const rows = list.sort((a: any, b: any) => {
+          const aMoment = moment(a.created);
+          const bMoment = moment(b.created);
+          if (aMoment.isSame(bMoment, 'day')) {
+            if (a.type === 'debit') {
+              return -1;
+            } else {
+              if (aMoment.isAfter(bMoment)) {
+                return -1; // a to top
+              } else {
+                return 1;
+              }
+            }
           } else {
             if (aMoment.isAfter(bMoment)) {
-              return -1; // a to top
+              return -1;
             } else {
               return 1;
             }
           }
-        } else {
-          if (aMoment.isAfter(bMoment)) {
-            return -1;
-          } else {
-            return 1;
-          }
-        }
-      });
+        });
 
-      this.dataSource = new MatTableDataSource(rows);
-      this.dataSource.sort = this.sort;
+        this.dataSource = new MatTableDataSource(rows);
+        this.dataSource.sort = this.sort;
+      });
     });
   }
 }
